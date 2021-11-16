d45e2a043fa9a6a794d4c563f758059eebbc9d8c
Valentin Polikarpov <valpolik@mail.ru>
2021-11-16 15:29:49 +0300

    Файлы переименованы в соответствии с традиционным форматом

--- init.rb
+++ init.rb
@@ -1,7 +1,10 @@
-require_relative 'personal_card_class'
+require_relative 'personal_card'
 
 puts "Вы должны ввести данные для заполнения визитной карточки"
 
 personal_card = PersonalCard.new
 
-personal_card.create_html_doc
+# personal_card.create_html_doc
+
+
+personal_card.create_body
